52e17cef6f3ae27c92433b617c80d59d8a37547f
bgshelke <shelkebhagwat12@gmail.com>
2023-10-28 14:34:01 +0530

    model createed

--- project1/app1/models.py
+++ project1/app1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Enquiry(models.Model):
+    sno=models.IntegerField(auto_created=True,primary_key=True)
+    name = models.CharField(max_length=80)
+    mobile = models.BigIntegerField()
+    education = models.CharField(max_length=80)
+    stream =models.CharField(max_length=80,null=True)
+    college = models.CharField(max_length=80,null=True)
+    gender = models.CharField(max_length=20)
+    pass_year = models.IntegerField()
+    course = models.CharField(max_length=80)
+    b_type = models.CharField(max_length=80)
+    reff = models.CharField(max_length=200)
+    remark = models.CharField(max_length=255)
+
--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .forms import EnquiryForm
 
 # Create your views here.
+
+def enquiry_view(request):
+    form = EnquiryForm()
+    template_name  = 'app1/enquiry.html'
+    context = {'form':form}
+    return render(request,template_name,context)
